c7c226f61dfa19965ab3127866e631ac5f0efdfb
JP <jplm289@gmail.com>
2023-12-03 21:46:24 -0600

    Initial commit

---
+++ app/src/main/java/com/example/inventoryappdb/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.inventoryappdb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.inventoryappdb.ui.theme.InventoryAppDBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InventoryAppDBTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/inventoryappdb/SortType.kt
@@ -0,0 +1,7 @@
+package com.example.inventoryappdb
+
+enum class SortType {
+    FIRST_NAME,
+    LAST_NAME,
+    PHONE_NUMBER
+}
